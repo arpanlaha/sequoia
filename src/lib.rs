d601b7b0c7d398535ea343350191a66ca4ef30f7
Arpan Laha <arpanlaha99@gmail.com>
2020-10-01 22:04:49 +0000

    initial struct structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+
+pub enum OctreeNode<T> {
+    Leaf(Option<T>),
+    Subtree(Box<[OctreeNode<T>; 8]>),
+}
+
+pub struct Octree<T> {
+    root: OctreeNode<T>,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
